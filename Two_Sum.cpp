bb967cddec85619921a2126fa6c8213c83aa1023
tanisha1402 <tanishachowdhury1402@gmail.com>
2025-10-01 23:18:48 +0600

    1. Two Sum - Array

---
+++ Two_Sum.cpp
@@ -0,0 +1,19 @@
+/*
+https://leetcode.com/problems/two-sum/
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            int com = target - nums[i];
+        auto it = mp.find(com);
+            if (it != mp.end()) {
+            return {it->second, i};
+            }
+
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
